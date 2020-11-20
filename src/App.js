deebfa599aaab3f193899a38526102ac53212efc
Igor Leão <igorleao.eng@gmail.com>
2020-11-19 23:08:09 -0300

    requeriment login

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
-    </BrowserRouter>
-
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
