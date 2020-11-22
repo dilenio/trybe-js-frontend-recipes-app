ece172ab3b5a2edff9288f7937ee0972134a415d
Dilenio Enderle <dilenio@gmail.com>
2020-11-22 15:35:56 -0300

    feat: 16. Redirect to the recipe details screen if only one recipe is found, with its ID in the URL

--- src/services/API.js
+++ src/services/API.js
@@ -5,33 +5,32 @@ const INGREDIENT_DRINK_API = 'https://www.thecocktaildb.com/api/json/v1/1/filter
 const NAME_DRINK_API = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const FIRSTLETTER_DRINK_API = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
 
+const fetchData = async (url, endpoint, type) => {
+  const data = await (await fetch(`${url}${endpoint}`)).json();
+  return data[type];
+};
+
 export const getMealsAPI = async (searchText, searchType) => {
   if (searchType === 'ingredient') {
-    const data = await (await fetch(`${INGREDIENT_MEAL_API}${searchText}`)).json();
-    return data.meals;
+    return fetchData(INGREDIENT_MEAL_API, searchText, 'meals');
   }
   if (searchType === 'name') {
-    const data = await (await fetch(`${NAME_MEAL_API}${searchText}`)).json();
-    return data.meals;
+    return fetchData(NAME_MEAL_API, searchText, 'meals');
   }
   if (searchType === 'firstletter') {
-    const data = await (await fetch(`${FIRSTLETTER_MEAL_API}${searchText}`)).json();
-    return data.meals;
+    return fetchData(FIRSTLETTER_MEAL_API, searchText, 'meals');
   }
 };
 
 export const getDrinksApi = async (searchText, searchType) => {
   if (searchType === 'ingredient') {
-    const data = await (await fetch(`${INGREDIENT_DRINK_API}${searchText}`)).json();
-    return data.drinks;
+    return fetchData(INGREDIENT_DRINK_API, searchText, 'drinks');
   }
   if (searchType === 'name') {
-    const data = await (await fetch(`${NAME_DRINK_API}${searchText}`)).json();
-    return data.drinks;
+    return fetchData(NAME_DRINK_API, searchText, 'drinks');
   }
   if (searchType === 'firstletter') {
-    const data = await (await fetch(`${FIRSTLETTER_DRINK_API}${searchText}`)).json();
-    return data.drinks;
+    return fetchData(FIRSTLETTER_DRINK_API, searchText, 'drinks');
   }
 };
 
