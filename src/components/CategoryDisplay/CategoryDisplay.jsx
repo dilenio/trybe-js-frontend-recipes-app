e96dfb0852f1295e3c05499ee1896c4a7d1447da
Anderson <andersoni.trybe@gmail.com>
2020-11-23 18:03:14 -0300

    useEffect for Api request

--- src/components/CategoryDisplay/CategoryDisplay.jsx
+++ src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { useEffect } from 'react';
+import Context from '../../context/Context';
+import { getMealsAPI, getDrinksApi } from '../../services/API';
 import './CategoryDisplay.css';
 
 const CategoryDisplay = () => {
 
+  const [categoryes, setCategoryes] = useState([]);
 
-  
+  const { pageTitle } = useContext(Context);
+
+  useEffect(() => {
+    if (pageTitle === 'Comidas') {
+      getMealsAPI('list', 'category').then((data) => {
+        setCategoryes(data);
+      });
+    } else if (pageTitle === 'Bebidas') {
+      getDrinksApi('list', 'category').then((data) => {
+        setCategoryes(data);
+      });
+    }
+  }, [pageTitle]);
+
   return (
     <div>
       Categorias
       <tr>
-        <td>Bife</td>
+        <td>
+          <button
+            type="button"
+            onClick={ (event) => console.log(categoryes) }
+          >
+            Teste
+          </button>
+        </td>
         <td>Melão</td>
       </tr>
     </div>
