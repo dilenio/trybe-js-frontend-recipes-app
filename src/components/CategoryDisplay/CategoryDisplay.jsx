3f3619ec4a60e1b1f59d36149ef76caebb4ff86e
Anderson <andersoni.trybe@gmail.com>
2020-11-23 17:37:26 -0300

    Category display Pre build

---
+++ src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CategoryDisplay.css';
+
+const CategoryDisplay = () => {
+
+
+  
+  return (
+    <div>
+      Categorias
+      <tr>
+        <td>Bife</td>
+        <td>Melão</td>
+      </tr>
+    </div>
+  );
+};
+
+export default CategoryDisplay;
