c76a5789e64db30061da858cc5fef58c985183af
Anderson <andersoni.trybe@gmail.com>
2020-11-27 15:34:46 -0300

    Context test

--- src/pages/InProgress/InProgress.jsx
+++ src/pages/InProgress/InProgress.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../context/Context';
 import './InProgress.css';
 
 const InProgress = () => {
-
+  const { inProgressId } = useContext(Context);
   return (
     <div>
       In progress recipe
+      { inProgressId.id }
+      { inProgressId.type }
     </div>
-  )
-}
+  );
+};
 
 export default InProgress;
