b0d88836e1c8025c677340068376169b20c4cc97
Anderson <andersoni.trybe@gmail.com>
2020-11-27 16:31:48 -0300

    Api fetchs

--- src/pages/InProgress/InProgress.jsx
+++ src/pages/InProgress/InProgress.jsx
@@ -1,15 +1,28 @@
-import React, { useContext } from 'react';
-import { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Context from '../../context/Context';
+import { getMealsAPI, getDrinksApi } from '../../services/API';
 import './InProgress.css';
 
 const InProgress = () => {
   const { inProgressId } = useContext(Context);
+  const [data, setData] = useState([]);
 
   const recipeId = inProgressId.id;
   const recipeType = inProgressId.type;
 
   useEffect(() => {
+    if (recipeType === 'comidas' && recipeId !== '') {
+      getMealsAPI(recipeId, 'details').then((response) => {
+        setData(response[0]);
+      });
+    } else if (recipeType === 'bebidas' && recipeId !== '') {
+      getDrinksApi(recipeId, 'details').then((response) => {
+        setData(response[0]);
+      });
+    } else {
+      console.warn('fetch error');
+      return undefined;
+    }
   }, []);
 
   return (
@@ -17,6 +30,7 @@ const InProgress = () => {
       In progress recipe
       { recipeId || 'not fetch' }
       { recipeType || 'not fetch' }
+      { data.strMeal }
     </div>
   );
 };
