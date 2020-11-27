6e10e72c0e6c235d4c8a662d4e88463e823ea635
Anderson <andersoni.trybe@gmail.com>
2020-11-27 16:02:39 -0300

    Prebuild Id logic

--- src/pages/InProgress/InProgress.jsx
+++ src/pages/InProgress/InProgress.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
+import { useEffect } from 'react';
 import Context from '../../context/Context';
 import './InProgress.css';
 
 const InProgress = () => {
   const { inProgressId } = useContext(Context);
+
+  const recipeId = inProgressId.id;
+  const recipeType = inProgressId.type;
+
+  useEffect(() => {
+  }, []);
+
   return (
-    <div>
+    <div className="in-progress-wrapper">
       In progress recipe
-      { inProgressId.id }
-      { inProgressId.type }
+      { recipeId || 'not fetch' }
+      { recipeType || 'not fetch' }
     </div>
   );
 };
