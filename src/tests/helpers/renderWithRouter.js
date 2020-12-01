e49f6ccb96f2e94a72afeef800d7f9b55359050d
rafaelgelatti <rafael.gelatti@gmail.com>
2020-12-01 16:54:55 -0300

    profile page tests

--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-
+import Provider from '../../context/Provider';
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
-  return ({ ...render(<Router history={ history }>{ component }</Router>), history });
+  return ({
+    ...render(
+      <Router history={ history }>
+        <Provider>
+          { component }
+        </Provider>
+      </Router>,
+    ),
+    history,
+  });
 };
-
 export default renderWithRouter;
